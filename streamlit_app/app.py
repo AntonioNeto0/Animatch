b76f7b385f6771421d54b636f0cf484139e80d4a
caiomizan <caiomizan1@gmail.com>
2022-03-09 01:56:39 -0300

    Ajustes

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,20 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-ds = pd.read_csv("../resources_dataset/dataset/Anime.csv")
 projectName = "Anime Bucket"
+ds = pd.read_csv("../resources_dataset/dataset/Anime.csv")
+
+
+
 
 def main():
     st.set_page_config(page_title=projectName,page_icon="🌊")
     st.title(projectName)
     st.dataframe(ds)
+    st.write('Média de Rating = ' + str(round(ds['Rating'].mean(),4)))
+    st.markdown('**TIPOS DE ANIME**')
+
+    #Substituindo a Release_season FALL por AUTUMN
+    ds['Release_season'].replace(['Fall'],'Autumn',inplace=True)
+
+    #Plotandao graficos pela barra lateral
+    grafico = st.sidebar.selectbox('Gaficos',['Type','Release_season'])
+    ds_type = ds[grafico].value_counts().plot(kind = 'barh')
+    st.pyplot(ds_type.figure)
 
 if __name__ == '__main__':
     main()
-
-st.write('Média de Rating = ' + str(round(ds['Rating'].mean(),4)))
-st.markdown('**TIPOS DE ANIME**')
-
-grafico = st.sidebar.selectbox('Gaficos',['Type','Release_season'])
-ds_type = ds[grafico].value_counts().plot(kind = 'bar')
-st.pyplot(ds_type.figure)
